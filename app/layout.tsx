fd1d3350ba67051630e632c1f44e126671f53c1d
Arthur de Paula Correa <arthurdepaulacorrea@hotmail.com>
2025-05-21 16:40:27 -0300

    Sistema nivel 1

--- app/layout.tsx
+++ app/layout.tsx
@@ -36,7 +36,7 @@ export default function RootLayout({
         baseTheme: neobrutalism,
       }}
     >
-      <html lang="en">
+      <html lang="pt-br">
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
